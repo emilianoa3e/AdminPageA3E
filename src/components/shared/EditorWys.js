5c74e39770c6d8b05c5b215310195cdeab36722d
emilianoa3e <emiliano.ortiz@a3e.com.mx>
2023-06-09 13:00:54 -0600

    modified errors

--- src/components/shared/EditorWys.js
+++ src/components/shared/EditorWys.js
@@ -1,33 +1,54 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import instance from '../../shared/Axios';
 import { Button } from 'react-bootstrap';
 import Prueba from '../getWysInfo';
+import { Editor } from '@tinymce/tinymce-react';
+
 
 
 
 function EditorWys() {
 
-    
-
-    const [content, setContent] = useState('');
-
-    const handleChange = (value) => {
-        setContent(value);
-        console.log(content)
-
-    };
-
-    const handleOnSubmit = async () => {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
     }
+  };
 
 
-    return (
-      <>
-          <div>Holi</div>
-      </>
-    )
+  return (
+    <>
+      <Editor
+        apiKey="mx8xfbfszwym72a4mhwdhb2czmco4whjp4bls89y26ov210t"
+        initialValue="<p>Escribe aquí...</p>"
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: [
+            'advlist', 'anchor', 'autolink', 'autoresize', 'autosave',
+            'charmap', 'code', 'codesample', 'directionality',
+            'emoticons', 'fullscreen', 'help', 'hr', 'image', 'imagetools', 'importcss',
+            'insertdatetime', 'link', 'lists', 'media', 'nonbreaking',
+            'noneditable', 'pagebreak', 'paste', 'preview', 'print', 'quickbars', 'save',
+            'searchreplace', 'tabfocus', 'table',
+            'textpattern', 'toc', 'visualblocks', 'visualchars', 'wordcount'
+          ],
+          toolbar: 'undo redo | formatselect | ' +
+            'bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | ' +
+            'outdent indent | numlist bullist | ' +
+            'link unlink image | media | table | ' +
+            'forecolor backcolor | removeformat | code | fullscreen | help',
+        }}
+        onEditorChange={(content, editor) => {
+          console.log(content);
+        }}
+      />
+      
+    </>
+  );
 }
 
 export default EditorWys
